0b05335bfcd1d6f17e9977a658c4693d423d1769
Pkhang16 <gamingbyt123@gmail.com>
2026-01-25 00:23:26 +0700

    Final

--- Models/InvoiceTempItem.cs
+++ Models/InvoiceTempItem.cs
@@ -4,15 +4,14 @@
     {
         public string MaSP { get; set; }
         public string TenSP { get; set; }
-        public string MaLo { get; set; }  // Mã nội bộ (sinh tự động)
+        public string MaLo { get; set; }  
         public string DonVi { get; set; }
         public int SoLuong { get; set; }
         public decimal DonGia { get; set; }
 
-        // --- THÊM MỚI ---
         public string NSX { get; set; }
         public string HSD { get; set; }
-        // ----------------
+
         public decimal ThanhTien => SoLuong * DonGia;
     }
 }
